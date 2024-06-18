d360239cd229ed5c3e9c5d891bcfbec5eea6ccca
datlaxbro <khr7171@autuni.ac.nz>
2024-06-18 15:18:51 +1200

    learning Variables
    the start of learning Variables

---
+++ Learning-python/PythonIndentation.py
@@ -0,0 +1,41 @@
+if 5 > 2:
+    print("Five is Grater then two!")
+if 5 > 2:
+    print("Five is Grater then two!")
+
+#------------------------------------------------------------
+
+x = 5
+y = "Hello, World"
+
+print(x)
+print(y)
+
+#------------------------------------------------------------
+
+#This is a comment.
+print ("Hello, World!") # this is also a comment
+
+#------------------------------------------------------------
+
+"""
+i can type anything here and it wont run ad is treated just as commented text.
+thats cool.
+"""
+#------------------------------------------------------------
+
+a = 5
+b = "John"
+
+print(a)
+print(b)
+
+#------------------------------------------------------------
+
+# changing Variable types 
+x = 4 # x is now an int type
+x = "Selly" # x is now a str type
+print(x)
+
+#------------------------------------------------------------
+
